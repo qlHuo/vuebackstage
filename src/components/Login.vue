f080fa9cc5defe5b0e9cc274070028b59333cd4e
qlHuo <1322346367@qq.com>
2020-05-09 23:58:17 +0800

    完成了登录功能

---
+++ src/components/Login.vue
@@ -0,0 +1,148 @@
+<template>
+  <div class="login_container">
+    <div class="login_box">
+      <!-- 头像区域 -->
+      <div class="avatar_box">
+        <img src="../assets/logo.png" alt="头像" />
+      </div>
+
+      <!-- 登录表单区域 -->
+      <el-form
+        ref="loginFormRef"
+        label-width="0px"
+        :model="loginForm"
+        :rules="loginFormRules"
+        class="login_form"
+      >
+        <el-form-item prop="username">
+          <!-- 用户名 -->
+          <el-input v-model="loginForm.username" prefix-icon="iconfont icon-user"></el-input>
+        </el-form-item>
+        <!-- 密码 -->
+        <el-form-item prop="password">
+          <el-input
+            v-model="loginForm.password"
+            type="password"
+            prefix-icon="iconfont icon-3702mima"
+          ></el-input>
+        </el-form-item>
+        <!-- 按钮区域 -->
+        <el-form-item class="btns">
+          <el-button type="primary" @click="login">登录</el-button>
+          <el-button type="info" @click="resetLoginForm">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      // 这谁登录表单的数据绑定
+      loginForm: {
+        username: 'admin',
+        password: '123456'
+      },
+      //   这是表单的验证规则对象
+      loginFormRules: {
+        //   用户名验证
+        username: [
+          { required: true, message: '请输入用户名', trigger: 'blur' },
+          { min: 3, max: 10, message: '长度在 3 到 10 个字符', trigger: 'blur' }
+        ],
+        // 密码验证
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' },
+          { min: 6, max: 15, message: '长度在 6 到 15 个字符', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  methods: {
+    //   点击重置按钮，重置登录表单
+    resetLoginForm() {
+      this.$refs.loginFormRef.resetFields()
+      console.log(this.$refs)
+    },
+    login() {
+      this.$refs.loginFormRef.validate(async valid => {
+        // console.log(valid);
+        // 如果valid为false，不发起请求
+        if (!valid) return
+        const { data: res } = await this.$http.post('login', this.loginForm)
+        // 获取返回的信息
+        // console.log(res);
+
+        if (res.meta.status !== 200) {
+          return this.$message.error('登录失败')
+        }
+        this.$message.success('登录成功')
+
+        // 1.将登陆成功之后得token，保存到客户端的sessionStorage中
+        //1.1 项目中除了登陆之外的其他API接口，必须在登录之后才能访问
+        // 1.2 token只应当在当前网站打开期间生效，所以将token保存在sessionStorage中
+        // console.log(res);
+        //将token保存到session中
+        window.sessionStorage.setItem('token', res.data.token)
+        //2. 通过编程式导航跳转到后台主页，路由地址是 /home
+        this.$router.push('./home')
+      })
+    }
+  }
+}
+</script>
+
+// 支持less scoped设置样式在当前vue单文件组件中生效
+<style lang="less" scoped>
+.login_container {
+  background-color: #2b4b6b;
+  height: 100%;
+}
+
+.login_box {
+  width: 450px;
+  height: 300px;
+  background-color: #fff;
+  border-radius: 3px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+
+  .avatar_box {
+    height: 130px;
+    width: 130px;
+    border-radius: 50%;
+    border: 1px solid #eee;
+    padding: 10px;
+    position: absolute;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: #fff;
+    box-shadow: 0 0 10px #ddd;
+
+    img {
+      //   display: block;
+      // margin: 25px auto;
+      width: 100%;
+      height: 100%;
+      border-radius: 50%;
+      background-color: #eee;
+    }
+  }
+}
+.login_form {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  padding: 0 20px;
+  box-sizing: border-box;
+}
+
+.btns {
+  display: flex;
+  justify-content: flex-end;
+}
+</style>
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,11 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
+import { Button, Form, FormItem, Input } from 'element-ui'
+// 导入弹框提示组件
+import { Message } from 'element-ui'
 
 Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+// 把Message组件挂载到Vue上，可以通过this直接访问Message
+Vue.prototype.$message = Message

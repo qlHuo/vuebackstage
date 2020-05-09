a2776824715b80c870e2ecbdd4a1d00108766c20
qlHuo <1322346367@qq.com>
2020-05-10 00:39:57 +0800

    重新提交登录功能代码

--- src/router/index.js
+++ src/router/index.js
@@ -3,13 +3,20 @@ import VueRouter from 'vue-router'
 import Login from '../components/Login.vue'
 import Home from '../components/Home.vue'
 
-
 Vue.use(VueRouter)
 
-const routes = [
-  { path: '/', redirect: '/login' },
-  { path: '/login', component: Login },
-  { path: '/home', component: Home }
+const routes = [{
+  path: '/',
+  redirect: '/login'
+},
+{
+  path: '/login',
+  component: Login
+},
+{
+  path: '/home',
+  component: Home
+}
 ]
 
 const router = new VueRouter({
@@ -18,16 +25,14 @@ const router = new VueRouter({
 
 // 挂载路由导航守卫
 router.beforeEach((to, from, next) => {
-  //to  将要访问的路径
-  //form  代表从那个路径跳转而来
-  //next  是一个函数，表示放行，直接放行next(), 强制跳转next("/login")
-
-  if (to.path === '/login') return next();
-  //获取token
+  // to  将要访问的路径
+  // form  代表从那个路径跳转而来
+  // next  是一个函数，表示放行，直接放行next(), 强制跳转next("/login")
+  if (to.path === '/login') return next()
+  // 获取token
   const tokenStr = window.sessionStorage.getItem('token')
-  if (!tokenStr) return next('/login');
+  if (!tokenStr) return next('/login')
   next()
-
 })
 
 export default router
